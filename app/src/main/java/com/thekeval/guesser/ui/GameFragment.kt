d086f4b8e3e4c3e9111a97c4b3b79f61e7571607
Keval <keval.langalia@live.com>
2021-05-11 23:02:34 -0400

    step ahead

--- app/src/main/java/com/thekeval/guesser/ui/GameFragment.kt
+++ app/src/main/java/com/thekeval/guesser/ui/GameFragment.kt
@@ -11,28 +11,21 @@ import androidx.databinding.DataBindingUtil
 import com.thekeval.guesser.R
 import com.thekeval.guesser.databinding.FragmentGameBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [GameFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class GameFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     lateinit var binding: FragmentGameBinding
+    var gameStarted = false
+    var pickedNumber: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -43,7 +36,6 @@ class GameFragment : Fragment() {
 
         val etNumber = binding.etNumber
         val btnHinde = binding.btnHide
-        val viewHide = binding.viewHide
 
         btnHinde.setOnClickListener {
             val btnHide = binding.btnHide
@@ -52,6 +44,11 @@ class GameFragment : Fragment() {
                 binding.viewHide.visibility = View.VISIBLE
                 btnHide.text = "Show"
                 binding.etNumber.isEnabled = false
+
+                if (!gameStarted) {
+                    pickedNumber = etNumber.text.toString().toInt()
+                    proceed();
+                }
             }
             else if (btnHide.text.toString().toLowerCase() == "show") {
                 binding.viewHide.visibility = View.GONE
@@ -63,22 +60,11 @@ class GameFragment : Fragment() {
         return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GameFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                GameFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+    private fun proceed() {
+        binding.txtInstructions.visibility = View.VISIBLE
+        binding.etSeekerNumber.visibility = View.VISIBLE
+        binding.btnCheck.visibility = View.VISIBLE
+        binding.rvGuesses.visibility = View.VISIBLE
     }
+
 }
